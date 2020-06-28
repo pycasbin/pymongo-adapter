aa748d48286b805aeb62107cc8a2b369130d5c65
Andy Xia <118010339@link.cuhk.edu.cn>
2020-06-28 22:04:14 +0800

    Add the newest version of adapter.py and test_adapter.py

--- casbin_pymongo_adapter/adapter.py
+++ casbin_pymongo_adapter/adapter.py
@@ -1,35 +1,146 @@
 from casbin import persist
 from pymongo import MongoClient
 
+class CasbinRule():
+    '''
+    CasbinRule model
+    '''
+
+    __tablename__ = "casbin_rule"
+
+    def __init__(self, ptype='', v0='', v1='', v2='', v3='', v4='', v5=''):
+        self.ptype = ptype
+        self.v0 = v0
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+        self.v4 = v4
+        self.v5 = v5
+
+    def __str__(self):
+        dict = {'ptype':self.ptype}
+        if self.v0:
+            dict['v0'] = self.v0
+        if self.v1:
+            dict['v1'] = self.v1
+        if self.v2:
+            dict['v2'] = self.v2
+        if self.v3:
+            dict['v3'] = self.v3
+        if self.v4:
+            dict['v4'] = self.v4
+        if self.v5:
+            dict['v5'] = self.v5
+        return dict
+
+    def __repr__(self):
+        return '<CasbinRule :"{}">'.format(str(self))
+
 
 class Adapter(persist.Adapter):
-    """the pymongo adapter for Casbin."""
+    """the interface for Casbin adapters."""
 
     def __init__(self, uri, dbname, collection="casbin_rule"):
         client = MongoClient(uri)
         db = client[dbname]
         self._collection = db[collection]
-        pass
 
     def load_policy(self, model):
-        """loads all policy rules from the storage."""
+        '''
+        implementing add Interface for casbin \n
+        load all policy rules from mongodb \n
+        '''
         for lines in self._collection.find():
-            pass
+            persist.load_policy_line(str(lines), model)
+
+    def _save_policy_line(self, ptype, rule):
+        line = CasbinRule(ptype=ptype)
+        if len(rule) > 0:
+            line.v0 = rule[0]
+        if len(rule) > 1:
+            line.v1 = rule[1]
+        if len(rule) > 2:
+            line.v2 = rule[2]
+        if len(rule) > 3:
+            line.v3 = rule[3]
+        if len(rule) > 4:
+            line.v4 = rule[4]
+        if len(rule) > 5:
+            line.v5 = rule[5]
+        document = {
+            'ptype': line.ptype,
+            'v0': line.v0,
+            'v1': line.v1,
+            'v2': line.v2,
+            'v3': line.v3,
+            'v4': line.v4,
+            'v5': line.v5,
+        }
+        # if len(rule) == 1:
+        #     document = {'ptype':ptype,
+        #                 'v0':rule[0]
+        #                 }
+        # elif len(rule) == 2:
+        #     document = {'ptype': ptype,
+        #                 'v0': rule[0],
+        #                 'v1': rule[1]
+        #                 }
+        # elif len(rule) == 3:
+        #     document = {'ptype': ptype,
+        #                 'v0': rule[0],
+        #                 'v1': rule[1],
+        #                 'v2': rule[2]
+        #                 }
+        # elif len(rule) == 4:
+        #     document = {'ptype': ptype,
+        #                 'v0': rule[0],
+        #                 'v1': rule[1],
+        #                 'v2': rule[2],
+        #                 'v3': rule[3]
+        #                 }
+        # elif len(rule) == 5:
+        #     document = {'ptype': ptype,
+        #                 'v0': rule[0],
+        #                 'v1': rule[1],
+        #                 'v2': rule[2],
+        #                 'v3': rule[3],
+        #                 'v4': rule[4]
+        #                 }
+        # else:
+        #     document = {'ptype': ptype,
+        #                 'v0': rule[0],
+        #                 'v1': rule[1],
+        #                 'v2': rule[2],
+        #                 'v3': rule[3],
+        #                 'v4': rule[4],
+        #                 'v5': rule[5]
+        #                 }
+        # self._collection.insert_one(document)
+        self._collection.insert_one(line)
 
     def save_policy(self, model):
-        """saves all policy rules to the storage."""
-        pass
+        '''
+        implementing add Interface for casbin \n
+        save the policy in mongodb \n
+        '''
+        for sec in ["p", "g"]:
+            if sec not in model.model.keys():
+                continue
+            for ptype, ast in model.model[sec].items():
+                for rule in ast.policy:
+                    self._save_policy_line(ptype, rule)
+        return True
 
     def add_policy(self, sec, ptype, rule):
-        """adds a policy rule to the storage."""
-        pass
+        """add policy rules to mongodb"""
+        self._save_policy_line(ptype, rule)
 
     def remove_policy(self, sec, ptype, rule):
-        """removes a policy rule from the storage."""
+        """delete policy rules from mongodb"""
         pass
 
     def remove_filtered_policy(self, sec, ptype, field_index, *field_values):
-        """removes policy rules that match the filter from the storage.
-        This is part of the Auto-Save feature.
+        """
+        delete policy rules for matching filters from mongodb
         """
         pass
