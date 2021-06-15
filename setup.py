85cc1cf42d52e65d32f78df089ed9776e7721145
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-06-16 07:54:56 +0800

    ci: change travis-ci to github-action
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-import setuptools
+from setuptools import setup, find_packages, __version__
+from os import path
 
 desc_file = "README.md"
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
 with open(desc_file, "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+# get the dependencies and installs
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+
+
+setup(
     name="casbin_pymongo_adapter",
-    version="0.1.0",
     author="TechLee,Xhy-5000,AmosChenYQ",
     author_email="techlee@qq.com,amoschenyq@foxmail.com",
     description="PyMongo Adapter for PyCasbin",
@@ -15,8 +25,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pycasbin/pymongo-adapter",
     keywords=["casbin", "pymongo", "casbin-adapter", "rbac", "access control", "abac", "acl", "permission"],
-    packages=setuptools.find_packages(),
-    install_requires=['casbin>=0.8.4', 'pymongo>=3.10.1'],
+    packages=find_packages(),
+    install_requires=install_requires,
     python_requires=">=3.6",
     license="Apache 2.0",
     classifiers=[
