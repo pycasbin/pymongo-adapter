24443b9ede8dfba308331e3b87653701a96d7263
sanguinedab <68048125+sanguinedab@users.noreply.github.com>
2024-10-23 14:02:56 +0800

    Update adapter.py
    fix format

--- casbin_pymongo_adapter/adapter.py
+++ casbin_pymongo_adapter/adapter.py
@@ -165,7 +165,9 @@ class Adapter(persist.Adapter):
         if not (1 <= field_index + len(field_values) <= 6):
             return False
         query = {
-            f"v{index + field_index}": value for index, value in enumerate(field_values) if value != ""
+            f"v{index + field_index}": value 
+            for index, value in enumerate(field_values) 
+            if value != ""
         }
         query["ptype"] = ptype
         results = self._collection.delete_many(query)
